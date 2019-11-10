0761a9153011ff1efab4ddf9d850e17a83590c07
yannickw <yannickw@sicara.com>
2019-11-10 23:02:40 +0100

    Make images_to_video work with images of different sizes by padding them

--- utils/images_to_video.py
+++ utils/images_to_video.py
@@ -2,15 +2,35 @@ import cv2
 
 
 def images_to_video(images_paths, output_path, images_per_seconds=10, codec='MJPG'):
-    images = [cv2.imread(image_path) for image_path in images_paths]
-    height, width, _ = images[0].shape
+    images = []
+    max_height = 0
+    max_width = 0
+
+    for image_path in images_paths:
+        image = cv2.imread(image_path)
+        height, width, _ = image.shape
+        if height > max_height:
+            max_height = height
+        if width > max_width:
+            max_width = width
+        images.append(image)
 
     video_writer = cv2.VideoWriter(
         output_path,
         cv2.VideoWriter_fourcc(*codec),
         images_per_seconds,
-        (width, height)
+        (max_width, max_height)
     )
+
     for image in images:
-        video_writer.write(image)
+        height, width, _ = image.shape
+        padded_image = cv2.copyMakeBorder(
+            image,
+            top=0,
+            bottom=max(0, max_height - height),
+            left=0,
+            right=max(0, max_width - width),
+            borderType=cv2.BORDER_REPLICATE
+        )
+        video_writer.write(padded_image)
     video_writer.release()
