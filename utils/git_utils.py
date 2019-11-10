20a4235969caafecec0fc2d64c975ebe1e447a0a
ggrelet <ggrelet@users.noreply.github.com>
2019-11-10 21:26:54 +0100

    Appending modifications

--- utils/git_utils.py
+++ utils/git_utils.py
@@ -2,10 +2,10 @@ from git import Repo
 from pathlib import Path
 
 
-def get_file_data(repository_path, file_path, branch_name="master"):
+def get_file_history(repository_path, file_path, branch_name="master"):
     """
     Returns a list containing strings of the historical modifications of a file
-    from a given git repository.
+    from a given git repository (locally only).
 
     Args:
         repository_path (str): The path to a git repository (i.e. containing a .git folder).
@@ -13,20 +13,14 @@ def get_file_data(repository_path, file_path, branch_name="master"):
         branch_name (str): The branch name from which you want to fetch the textual history of your file.
 
     Returns:
-        file_history (list): The list containing the iterations of your file.
+        List[str]: The list containing the iterations of your file.
 
     """
-    if not isinstance(repository_path, Path):
-        repository_path = Path(repository_path)
+    repository_path = Path(repository_path)
 
     repo = Repo(repository_path)
     commits = list(repo.iter_commits(branch_name, paths=file_path))
-    file_history = []
-
-    for commit in commits:
-        # For each commit of the given branch name,
-        # get the content of the file and add it to file_history.
-        file_content = repo.git.show('{}:{}'.format(commit.hexsha, file_path))
-        file_history.append(file_content)
-
-    return file_history
+    return [
+        repo.git.show(f'{commit.hexsha}:{file_path}')
+        for commit in commits
+    ]
