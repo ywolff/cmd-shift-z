87130cdbefc45b91f7736fb19ad01de23a778a5c
ggrelet <ggrelet@users.noreply.github.com>
2019-11-10 23:22:46 +0100

    Modifying utils/code_to_html.py to accept string as html input

--- utils/code_to_html.py
+++ utils/code_to_html.py
@@ -1,32 +1,27 @@
-from os.path import splitext, relpath, dirname
+from os.path import relpath, dirname
 
 from pygments import highlight
 from pygments.formatters.html import HtmlFormatter
 from pygments.lexers import guess_lexer_for_filename
 
 
-def code_to_html(input_file_path, html_output_file_path=None, css_output_file_path=None):
+def code_to_html(input_code, filename, html_output_file_path, css_output_file_path):
     """
-    Generate highlighted code as html and css files from a code file.
+    Generate highlighted code as html and css files from a code file in a string.
 
     Args:
-        input_file_path (str): text input file path
+        input_code (str): text input (data).
+        filename (str): the name of the file to process.
         html_output_file_path (str): .html output file path. If not provided, save html with same name as input file.
         css_output_file_path (str): .css output file path. If not provided, save css with same name as input file.
     """
-    input_file_path_without_ext, _ = splitext(input_file_path)
-    html_output_file_path = html_output_file_path or f'{input_file_path_without_ext}.html'
-    css_output_file_path = css_output_file_path or f'{input_file_path_without_ext}.css'
-
-    with open(input_file_path, 'r') as input_file:
-        input_code = input_file.read()
-
     html_formatter = HtmlFormatter(
         style='monokai',
         full=True,
         cssfile=relpath(css_output_file_path, dirname(html_output_file_path))
     )
-    lexer = guess_lexer_for_filename(input_file_path, input_code)
+
+    lexer = guess_lexer_for_filename(filename, input_code)
 
     with open(html_output_file_path, 'w+') as html_file:
         highlight(input_code, lexer, html_formatter, html_file)
