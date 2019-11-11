b06d0d49e849e6bea6390c9508c30ccca643068c
yannickw <yannickw@sicara.com>
2019-11-11 04:57:44 +0100

    Add docstrings for generate_sub_versions

--- utils/generate_sub_versions.py
+++ utils/generate_sub_versions.py
@@ -6,6 +6,19 @@ DEFAULT_FREQUENCY = 1 / 30
 
 
 def generate_sub_versions(start_version, end_version, frequency=DEFAULT_FREQUENCY):
+    """
+    Generate intermediary versions between 2 versions of the same text, by applying successively 'insert' and 'delete'
+    modifications from the diff between start and end versions.
+
+    Args:
+        start_version (str): text version from which to start
+        end_version (str): text version to transform into
+        frequency (float): sampling ratio for sub-versions
+
+    Returns:
+        List[str]: list of sampled sub-versions, beginning by start version but not necessarily ending with end version
+
+    """
     sub_versions = [start_version]
     current_version = start_version
     while current_version != end_version:
@@ -36,6 +49,15 @@ def generate_sub_versions(start_version, end_version, frequency=DEFAULT_FREQUENC
 
 
 def generate_all_sub_versions_from_list(versions, frequency=DEFAULT_FREQUENCY):
+    """
+    Generate intermediary versions based on several checkpoint versions of the same text.
+    Args:
+        versions (List[str]): list of all checkpoint versions
+        frequency (float): sampling ratio for sub-versions
+
+    Returns:
+        (List[str]): list of all sampled sub-versions, including also all checkpoint versions
+    """
     sub_versions = [
         sub_version
         for version, next_version in zip(versions[:-1], versions[1:])
