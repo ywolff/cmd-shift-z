22a54d80a211751a9ed95168838632b608166378
yannickw <yannickw@sicara.com>
2019-11-11 19:01:53 +0100

    Make images_to_video zoom out

--- utils/images_to_video.py
+++ utils/images_to_video.py
@@ -12,34 +12,45 @@ def images_to_video(images_paths, output_path, images_per_second=10, codec='MJPG
         codec (str): codec in which to output the video
     """
     images = []
-    max_height = 0
     max_width = 0
-
     for image_path in images_paths:
         image = cv2.imread(image_path)
-        height, width, _ = image.shape
-        if height > max_height:
-            max_height = height
+        _, width, _ = image.shape
         if width > max_width:
             max_width = width
         images.append(image)
+
+    resized_images = []
+    max_width_until_now = 0
+    resized_max_height = 0
+    for image in images:
+        _, width, _ = image.shape
+        if width > max_width_until_now:
+            max_width_until_now = width
+        resize_ratio = max_width / max_width_until_now
+        resized_image = cv2.resize(image, (0, 0), fx=resize_ratio, fy=resize_ratio)
+        height, _, _ = resized_image.shape
+        if height > resized_max_height:
+            resized_max_height = height
+        resized_images.append(resized_image)
 
     video_writer = cv2.VideoWriter(
         output_path,
         cv2.VideoWriter_fourcc(*codec),
         images_per_second,
-        (max_width, max_height)
+        (max_width, resized_max_height)
     )
 
-    for image in images:
-        height, width, _ = image.shape
+    for resized_image in resized_images:
+        height, width, _ = resized_image.shape
         padded_image = cv2.copyMakeBorder(
-            image,
+            resized_image,
             top=0,
-            bottom=max(0, max_height - height),
+            bottom=max(0, resized_max_height - height),
             left=0,
             right=max(0, max_width - width),
-            borderType=cv2.BORDER_REPLICATE
+            borderType=cv2.BORDER_REPLICATE,
         )
+        print('padded:', padded_image.shape)
         video_writer.write(padded_image)
     video_writer.release()
