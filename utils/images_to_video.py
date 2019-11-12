58f8dba58a35ea91f0357087c6b77421433c724b
yannickw <yannickw@sicara.com>
2019-11-13 00:44:13 +0100

    Explain video zoom smoothing in docstring

--- utils/images_to_video.py
+++ utils/images_to_video.py
@@ -7,13 +7,18 @@ import cv2
 
 def images_to_video(images_paths, output_path, images_per_second=10, codec='MJPG', zoom_out_smoothing_window=5):
     """
-    Combine images in a video
+    Combine images of different sizes in a video
 
     Args:
         images_paths (List[str]): list of images paths to combine
         output_path (str): path to output the video
         images_per_second (int): number of frames per second
         codec (str): codec in which to output the video
+        zoom_out_smoothing_window (int): size of the window used to smooth zoom ratios with a sliding average, in order
+                                         to avoid sudden zoom changes.
+                                         For example, if you have images with widths [50, 50, 50, 100, 100, 100, 200],
+                                         the zoom ratios will be [4, 4, 4, 2, 2, 2, 1], and with a window of size 3, the
+                                         smoothed zoom ratios will be [4, 4, 3.33, 2.66, 2, 1.66, 1.33].
     """
     images = []
     max_width = 0
