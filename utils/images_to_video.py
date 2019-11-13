c126ffb49beef7a9c04753c69e8875e20e6e2c8c
yannickw <yannickw@sicara.com>
2019-11-13 01:01:48 +0100

    Refactor images_to_video

--- utils/images_to_video.py
+++ utils/images_to_video.py
@@ -20,23 +20,13 @@ def images_to_video(images_paths, output_path, images_per_second=10, codec='MJPG
                                          the zoom ratios will be [4, 4, 4, 2, 2, 2, 1], and with a window of size 3, the
                                          smoothed zoom ratios will be [4, 4, 3.33, 2.66, 2, 1.66, 1.33].
     """
-    images = []
-    max_width = 0
-    for image_path in images_paths:
-        image = cv2.imread(image_path)
-        _, width, _ = image.shape
-        if width > max_width:
-            max_width = width
-        images.append(image)
+    images = [cv2.imread(image_path) for image_path in images_paths]
 
-    resize_ratios = []
-    max_width_until_now = 0
-    for image in images:
-        _, width, _ = image.shape
-        if width > max_width_until_now:
-            max_width_until_now = width
-        resize_ratios.append(max_width / max_width_until_now)
+    widths = [image.shape[1] for image in images]
+    accumulated_max_widths = np.maximum.accumulate(widths)
+    max_width = accumulated_max_widths[-1]
 
+    resize_ratios = np.full(len(images), max_width) / accumulated_max_widths
     smooth_resize_ratios = np.convolve(
         np.pad(
             resize_ratios,
@@ -47,14 +37,23 @@ def images_to_video(images_paths, output_path, images_per_second=10, codec='MJPG
         mode='valid'
     )
 
-    resized_images = []
-    resized_max_height = 0
-    for image, resize_ratio in zip(images, smooth_resize_ratios):
-        resized_image = cv2.resize(image, (0, 0), fx=resize_ratio, fy=resize_ratio)
-        height, _, _ = resized_image.shape
-        if height > resized_max_height:
-            resized_max_height = height
-        resized_images.append(resized_image)
+    resized_images = [
+        cv2.resize(image, (0, 0), fx=resize_ratio, fy=resize_ratio)
+        for image, resize_ratio in zip(images, smooth_resize_ratios)
+    ]
+    resized_max_height = np.max([image.shape[0] for image in images])
+
+    padded_images = [
+        cv2.copyMakeBorder(
+            resized_image,
+            top=0,
+            bottom=max(0, resized_max_height - resized_image.shape[0]),
+            left=0,
+            right=max(0, max_width - resized_image.shape[1]),
+            borderType=cv2.BORDER_REPLICATE,
+        )
+        for resized_image in resized_images
+    ]
 
     video_writer = cv2.VideoWriter(
         output_path,
@@ -63,15 +62,6 @@ def images_to_video(images_paths, output_path, images_per_second=10, codec='MJPG
         (max_width, resized_max_height)
     )
 
-    for resized_image in resized_images:
-        height, width, _ = resized_image.shape
-        padded_image = cv2.copyMakeBorder(
-            resized_image,
-            top=0,
-            bottom=max(0, resized_max_height - height),
-            left=0,
-            right=max(0, max_width - width),
-            borderType=cv2.BORDER_REPLICATE,
-        )
+    for padded_image in padded_images:
         video_writer.write(padded_image)
     video_writer.release()
